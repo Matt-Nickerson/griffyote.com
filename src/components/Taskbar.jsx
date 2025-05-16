ffa19d949a41c2f7087fd10f2a29c96884c73837
Matt Nickerson <mattwnickerson39@gmail.com>
2025-05-16 01:20:05 -0400

    art page set up

--- src/components/Taskbar.jsx
+++ src/components/Taskbar.jsx
@@ -1,17 +1,12 @@
 import '../App.css';
 import griff from '../GriffCircleHead.png';
-import {Image} from '@heroui/image';
 import {Navbar,
     NavbarBrand, 
     NavbarContent, 
     NavbarItem, 
-    NavbarMenuToggle,
-    NavbarMenu,
-    NavbarMenuItem
 } from '@heroui/navbar';
 import {Link} from '@heroui/link';
 import {Button} from '@heroui/button';
-import {useState} from 'react';
 
 function Taskbar() {
 
@@ -20,8 +15,8 @@ function Taskbar() {
     <div>
     <Navbar shouldHideOnScroll>
       <NavbarBrand>
+      <img src={griff} alt="Griff" className=" mr-2 w-8 h-8 rounded-full" />
         <p className="font-semibold text-inherit">Griff Coyote </p>
-        <img src={griff} alt="Griff" className=" ml-2 w-8 h-8 rounded-full" />
       </NavbarBrand>
       <NavbarContent className="hidden sm:flex gap-4" justify="center">
         <NavbarItem>
