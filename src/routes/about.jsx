784d9585f23d20dc0d624d4857b7b0e65913dc18
Matt Nickerson <mattwnickerson39@gmail.com>
2025-05-17 05:23:05 -0400

    fixed sizing issue with grid

--- src/routes/about.jsx
+++ src/routes/about.jsx
@@ -9,12 +9,13 @@ const About = () => {
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.6 }}
       >
-        <div className="grid grid-cols-2 gap-12 mx-20 items-center justify-center">
+        <div className="grid grid-col-1 sm:grid-cols-2 gap-12 mx-20 items-center justify-center">
           <div>
             <img
               src="/fursuit/griffhednew1.png"
               alt="Griff Coyote"
-              className="scale-50"
+              className="sm:scale-50 scale-100"
+              loading="lazy"
             />
           </div>
           <div>
